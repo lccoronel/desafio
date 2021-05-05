ee14f3f7c7d97ebe17c115d350149ac80b560984
Lucas Coronel <lccoronel7@icloud.com>
2021-05-05 08:07:40 -0300

    challenge is ready

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,31 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTodo: Task = {
+      id: tasks.length + 1,
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldTasks => [...oldTasks, newTodo]);
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) task.isComplete = !task.isComplete;
+
+      return task;
+    });
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks);
   }
 
   return (
