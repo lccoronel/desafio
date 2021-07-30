f3bdeec6933e352c1acd9e2c01a46d36aceadbf6
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-07-30 17:00:27 -0300

    feat: stetment list

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -3,14 +3,14 @@ const { v4: uuidV4 } = require('uuid');
 
 const app = express();
 
-const costumers = [];
+const custumers = [];
 
 app.use(express.json());
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
-  const customerAlreadyExists = costumers.some(
+  const customerAlreadyExists = custumers.some(
     customer => customer.cpf === cpf,
   );
 
@@ -18,9 +18,17 @@ app.post('/account', (request, response) => {
     return response.status(400).json({ erro: 'costumer already exists' });
   }
 
-  costumers.push({ cpf, name, id: uuidV4(), statement: [] });
+  custumers.push({ cpf, name, id: uuidV4(), statement: [] });
 
   return response.status(201).send();
+});
+
+app.get('/stetment/:cpf', (request, response) => {
+  const { cpf } = request.params;
+
+  const { statement } = custumers.find(user => user.cpf === cpf);
+
+  return response.json({ statement });
 });
 
 app.listen(3333);
