48bd943dcdefb14c4c70190ecda3ee485b6b7a51
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-08-02 16:31:52 -0300

    feat: statement filtered by date

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -21,6 +21,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acumulator, operation) => {
+    if (operation.type === 'credit') {
+      return acumulator + operation.amount;
+    }
+
+    return acumulator - operation.amount;
+  }, 0);
+
+  return balance;
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -37,9 +49,59 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/stetment', verifyIfExistsAccountCPF, (request, response) => {
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
   return response.json({ statement: customer.statement });
+});
+
+app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer, query } = request;
+
+  const dateFormatted = new Date(`${query.date} 00:00`);
+
+  const statementFiltered = customer.statement.filter(
+    statement =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormatted).toDateString(),
+  );
+
+  return response.json(statementFiltered);
+});
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statmentOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit',
+  };
+
+  customer.statement.push(statmentOperation);
+
+  return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { body, customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < body.amount) {
+    return response.status(400).json({ error: 'Insulficient founds' });
+  }
+
+  const statmentOperation = {
+    description: 'withdraw',
+    amount: body.amount,
+    created_at: new Date(),
+    type: 'debit',
+  };
+
+  customer.statement.push(statmentOperation);
+  return response.status(201).send();
 });
 
 app.listen(3333);
