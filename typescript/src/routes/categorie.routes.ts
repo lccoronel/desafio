052cbc6b7abe863f5463884dc3f133ed3651027d
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-08-10 11:39:53 -0300

    feat: category model

--- typescript/src/routes/categorie.routes.ts
+++ typescript/src/routes/categorie.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
 
 import { Category } from '../Model/Category';
 
@@ -10,16 +9,13 @@ const categories: Category[] = [];
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category: Category = {
-    id: uuidV4(),
-    name,
-    description,
-    created_at: new Date(),
-  };
+  const category = new Category();
+
+  Object.assign(category, { name, description, created_at: new Date() });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json(category);
 });
 
 export { categoriesRoutes };
