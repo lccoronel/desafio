f10dabe577559d7cd2963808f0dc3dba814e915a
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-08-10 07:19:31 -0300

    feat: setup debug

--- typescript/src/server.ts
+++ typescript/src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello world' }));
+app.get('/', (request, response) => {
+  const message = 'Hello world';
+  return response.json({ message });
+});
 
 app.listen(3333, () => console.log('Server is running on port 3333 🚀'));
