214fa6e6bd9998c021d784b8fac72236de531d63
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-08-13 16:20:40 -0300

    feat: specification

--- typescript/src/modules/cars/services/CreateSpecifiactionService.ts
+++ typescript/src/modules/cars/services/CreateSpecifiactionService.ts
@@ -1,6 +1,20 @@
+import {
+  ICreateSpecificationDTO,
+  ISpecificationRepository,
+} from '../repositories/ISpecificationRepository';
+
 class CreateSpecificationService {
-  execute() {
-    console.log('TODO');
+  constructor(private specificationsRepository: ISpecificationRepository) {}
+
+  execute({ name, description }: ICreateSpecificationDTO): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByname(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error('Specification already exists');
+    }
+
+    this.specificationsRepository.create({ name, description });
   }
 }
 
