f94227382ad3e44ca8afcfc6fb88bca45ccd6f6e
Lucas Coronel <lccoronel7@icloud.com>
2021-09-07 11:56:20 -0300

    feat: list categories dependencie injection

--- typescript/src/routes/categories.routes.ts
+++ typescript/src/routes/categories.routes.ts
@@ -3,19 +3,18 @@ import multer from 'multer';
 
 import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
 import { ImportCategoryController } from '../modules/cars/useCases/importCategory/ImportCategoryController';
-import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import { ListCategoriesController } from '../modules/cars/useCases/listCategories/ListCategoriesController';
 
 const categoriesRoutes = Router();
 const upload = multer({ dest: './tmp' });
 
 const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
+const listCategoriesController = new ListCategoriesController();
 
 categoriesRoutes.post('/', createCategoryController.handle);
 
-categoriesRoutes.get('/', (request, response) => {
-  return listCategoriesController.handle(request, response);
-});
+categoriesRoutes.get('/', listCategoriesController.handle);
 
 categoriesRoutes.post(
   '/import',
