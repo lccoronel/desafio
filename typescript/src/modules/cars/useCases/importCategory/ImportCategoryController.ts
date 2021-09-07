ca504ed86e80953e3256d5f5eca4d87d9dc06528
Lucas Coronel <lccoronel7@icloud.com>
2021-09-07 11:43:03 -0300

    feat: import categories dependencie injection

--- typescript/src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ typescript/src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
 class ImportCategoryController {
-  constructor(private importCategoryusecase: ImportCategoryUseCase) {}
-
   handle(request: Request, response: Response): Response {
     const { file } = request;
 
-    this.importCategoryusecase.execute(file);
+    const importCategoryUseCase = container.resolve(ImportCategoryUseCase);
+
+    importCategoryUseCase.execute(file);
 
     return response.send();
   }
--- typescript/src/routes/categories.routes.ts
+++ typescript/src/routes/categories.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
-import { importCategoryController } from '../modules/cars/useCases/importCategory';
+import { ImportCategoryController } from '../modules/cars/useCases/importCategory/ImportCategoryController';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
 const upload = multer({ dest: './tmp' });
 
 const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
 
 categoriesRoutes.post('/', createCategoryController.handle);
 
@@ -16,8 +17,10 @@ categoriesRoutes.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
 });
 
-categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-  return importCategoryController.handle(request, response);
-});
+categoriesRoutes.post(
+  '/import',
+  upload.single('file'),
+  importCategoryController.handle,
+);
 
 export { categoriesRoutes };
