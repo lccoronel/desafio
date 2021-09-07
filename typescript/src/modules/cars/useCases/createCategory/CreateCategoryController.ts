9652e30ece40aa28628ef4ddaec318e20bd0e7f1
Lucas Coronel <lccoronel7@icloud.com>
2021-09-07 10:37:23 -0300

    refactor: category useCase

--- typescript/src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ typescript/src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,14 +1,19 @@
 import { Response, Request } from 'express';
+import { inject, injectable } from 'tsyringe';
 
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
+@injectable()
 class CreateCategoryController {
-  constructor(private createCategoryService: CreateCategoryUseCase) {}
+  constructor(
+    @inject('CategoriesRepository')
+    private createCategoryService: CreateCategoryUseCase,
+  ) {}
 
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
-    this.createCategoryService.execute({ name, description });
+    await this.createCategoryService.execute({ name, description });
 
     return response.status(201).send();
   }
--- typescript/src/modules/cars/useCases/createCategory/index.ts
+++ typescript/src/modules/cars/useCases/createCategory/index.ts
@@ -2,10 +2,12 @@ import { CategoriesRepository } from '../../repositories/implementations/Categor
 import { CreateCategoryController } from './CreateCategoryController';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
-const categoriesRepository = CategoriesRepository.getInstance();
-const createCategoryService = new CreateCategoryUseCase(categoriesRepository);
-const createCategoryController = new CreateCategoryController(
-  createCategoryService,
-);
+export default (): CreateCategoryController => {
+  const categoriesRepository = new CategoriesRepository();
+  const createCategoryService = new CreateCategoryUseCase(categoriesRepository);
+  const createCategoryController = new CreateCategoryController(
+    createCategoryService,
+  );
 
-export { createCategoryController };
+  return createCategoryController;
+};
