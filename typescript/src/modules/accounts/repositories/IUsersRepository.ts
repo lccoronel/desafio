ecb9a6ff1363c46a9f1a51cbc68533e2db98074f
Lucas Coronel <lccoronel7@icloud.com>
2021-09-14 07:52:05 -0300

    ✨ : find by id repository

--- typescript/src/modules/accounts/repositories/IUsersRepository.ts
+++ typescript/src/modules/accounts/repositories/IUsersRepository.ts
@@ -4,6 +4,7 @@ import { User } from '../entities/User';
 interface IUsersReposiotry {
   create(data: ICreateUsersDTO): Promise<void>;
   filterByEmail(email: string): Promise<User | null>;
+  findById(id: string): Promise<User | null>;
 }
 
 export { IUsersReposiotry };
