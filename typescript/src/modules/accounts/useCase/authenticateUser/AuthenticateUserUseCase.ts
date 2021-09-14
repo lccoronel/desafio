e6d3f5486ca132e855bd238a6af3a6b9755b2304
Lucas Coronel <lccoronel7@icloud.com>
2021-09-14 07:13:39 -0300

    ✨ : authenticate user use case

---
+++ typescript/src/modules/accounts/useCase/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,49 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersReposiotry } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    email: string;
+    password: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersReposiotry: IUsersReposiotry,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersReposiotry.filterByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const passswordMatch = await compare(password, user.password);
+
+    if (!passswordMatch) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const token = sign({}, 'a7e071b3de48cec1dd24de6cbe6c7bf1', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export { AuthenticateUserUseCase };
