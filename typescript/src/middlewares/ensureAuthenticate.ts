5c0a9b420421e96d57c7565bb16f6229be53a18c
Lucas Coronel <lccoronel7@icloud.com>
2021-09-21 19:46:50 -0300

    refactor: middlewares

--- typescript/src/middlewares/ensureAuthenticate.ts
+++ typescript/src/middlewares/ensureAuthenticate.ts
@@ -35,6 +35,10 @@ async function ensureAuthenticate(
       throw new AppError('User does not exists', 401);
     }
 
+    request.user = {
+      id: user_id,
+    };
+
     next();
   } catch (error) {
     throw new AppError('Invalid token', 401);
