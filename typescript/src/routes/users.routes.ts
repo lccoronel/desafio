1ad9950a52b77bc35f46961dbc6f212fe6b9636b
Lucas Coronel <lccoronel7@icloud.com>
2021-09-21 19:47:12 -0300

    feat: users route

--- typescript/src/routes/users.routes.ts
+++ typescript/src/routes/users.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '../modules/accounts/useCase/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCase/updateUserAvatar/UpdateUserAvatarController';
 
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRouter.post('/', createUserController.handle);
+
+usersRouter.patch('/avatar', updateUserAvatarController.handle);
 
 export { usersRouter };
