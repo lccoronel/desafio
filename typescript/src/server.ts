0f6ef07d534a2d1deb92c6d6d633edd0109f5f48
Lucas Coronel <lccoronel7@icloud.com>
2022-01-30 09:02:19 -0300

    ✅ authenticate user use case tests

--- typescript/src/server.ts
+++ typescript/src/server.ts
@@ -16,17 +16,15 @@ app.use(express.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(router);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
-    }
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({ message: err.message });
+  }
 
-    return response.status(500).json({
-      status: 'error',
-      message: `internal server error - ${err.message}`,
-    });
-  },
-);
+  return response.status(500).json({
+    status: 'error',
+    message: `internal server error - ${err.message}`,
+  });
+});
 
 app.listen(3333, () => console.log('Server is running on port 3333 🚀'));
