07995020a8ed820d2873b89a78bc174cdd5a281c
Lucas Coronel <lccoronel7@icloud.com>
2022-02-06 10:59:12 -0300

    🚧 list cars module

--- typescript/src/modules/cars/repositories/ICarsRepository.ts
+++ typescript/src/modules/cars/repositories/ICarsRepository.ts
@@ -5,4 +5,5 @@ export interface ICarsRepository {
   cars?: Car[];
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
+  findAvailable(name?: string, brand?: string, category_id?: string): Promise<Car[]>;
 }
--- typescript/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ typescript/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -17,4 +17,17 @@ export class CarsRepositoryInMemory implements ICarsRepository {
   async findByLicensePlate(license_plate: string): Promise<Car> {
     return this.cars.find(car => car.license_plate === license_plate);
   }
+
+  async findAvailable(name?: string, brand?: string, category_id?: string): Promise<Car[]> {
+    const listAvailableCars = this.cars
+      .filter(car => car.available === true)
+      .filter(
+        availableCar =>
+          (name && availableCar.name === name) ||
+          (brand && availableCar.brand === brand) ||
+          (category_id && availableCar.category_id === category_id),
+      );
+
+    return listAvailableCars;
+  }
 }
