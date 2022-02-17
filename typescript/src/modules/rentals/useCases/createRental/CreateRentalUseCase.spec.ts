4ed22b1a990a9c8c666d20efe9085d0d1e2e0e06
Lucas Coronel <lccoronel7@icloud.com>
2022-02-17 07:24:54 -0300

    ✨ date provider

--- typescript/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ typescript/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,16 +1,19 @@
 import dayjs from 'dayjs';
 
 import { RentalsRepositoryInMemory } from '@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory';
+import { DayjsDateProvider } from '@shared/container/providers/implementations/DayjsDateProvider';
 import { AppError } from '@shared/errors/AppError';
 import { CreateRentalUseCase } from './CreateRentalUseCase';
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let dayjsDateProvider: DayjsDateProvider;
 
 describe('Create Rental', () => {
   beforeEach(() => {
     rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
-    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+    dayjsDateProvider = new DayjsDateProvider();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory, dayjsDateProvider);
   });
 
   it('should be able to create a new rental', async () => {
