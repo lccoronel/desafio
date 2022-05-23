52e3f834a5e5e2724327c3798c284b2c1dae9893
Lucas Coronel <lccoronel7@icloud.com>
2022-05-23 18:22:44 -0300

    ✨ deploy lambda and certificate template

--- certificateignite/src/functions/generateCertificate.ts
+++ certificateignite/src/functions/generateCertificate.ts
@@ -1,10 +1,30 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
+import * as dayjs from "dayjs"
+import { readFileSync } from "fs"
+import { compile } from 'handlebars'
+import { join } from 'path'
+
 import { document } from '../utils/dynamoDBClient'
 
 interface ICreateCertificate {
   id: string
   name: string
   grade: string
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+
+function compileTemplate(data: ITemplate) {
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs")
+  const html = readFileSync(filePath, "utf-8")
+
+  return compile(html)(data)
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -20,6 +40,19 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     KeyConditionExpression: "id = :id",
     ExpressionAttributeValues: { ":id": id }
   }).promise()
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png")
+  const medal = readFileSync(medalPath, "base64")
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date: dayjs().format('DD/MM/YYYY'),
+    medal
+  }
+
+  const content = await compileTemplate(data)
 
   return {
     statusCode: 201,
